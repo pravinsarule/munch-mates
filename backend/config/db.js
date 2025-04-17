d70ad753a1c46bbf8b21440aa9095ed1495bd6d5
Pravin Sarule <pravinsarule617@gmail.com>
2025-04-18 00:31:11 +0530

    initial commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,10 +7,13 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
   .then(() => console.log("📦 Connected to PostgreSQL"))
-  .catch((err) => console.error("Database connection error:", err));
+  .catch((err) => console.error("❌ Database connection error:", err));
 
 module.exports = pool;
